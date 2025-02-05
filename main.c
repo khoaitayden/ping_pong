53a2fd0ae8d21bb74c80367e2033058790dcac78
khoaitayden <nguyenkhoai9906@gmail.com>
2025-02-05 12:31:33 +0700

    Update 3
    add press M to mute or choose sound volume
    
    add other 2 soundtrack, press N to toggle between them (online track)

--- main.c
+++ main.c
@@ -15,6 +15,13 @@ bool modechoosed = false;
 float speedup=1.0f;
 Sound hitwall;
 Sound hitpad;
+bool mute=false;
+short songnum=1;
+float volume=0.1f;
+bool checkdarkmode=true;
+bool choosedtheme=false;
+float bothard=1;
+bool choosedbotmode=false;
 
 typedef struct Paddle {
     Rectangle rect;
@@ -29,8 +36,8 @@ typedef struct Ball {
 
 
 void padMove(Paddle *paddle, bool Up, bool Down){
-    if (Up && paddle->rect.y > 0) paddle->rect.y -= paddle->speed;
-    if (Down && paddle->rect.y < screenHeight - padHeight) paddle->rect.y += paddle->speed;
+    if (Up && paddle->rect.y > 0) paddle->rect.y -= paddle->speed*speedup;
+    if (Down && paddle->rect.y < screenHeight - padHeight) paddle->rect.y += paddle->speed*speedup;
 }
 
 void ballMove (Ball *ball, Paddle *paddle1, Paddle *paddle2){
@@ -82,14 +89,43 @@ void loadgame(Paddle *paddle1, Paddle *paddle2, Ball *ball){
 void runbot(Paddle *botpad, Ball *ball){
     if (ball->pos.y > botpad->rect.y + padHeight / 2) 
     {
-        botpad->rect.y += botpad->speed;
+        botpad->rect.y += botpad->speed*bothard;
     } 
     else 
         if (ball->pos.y < botpad->rect.y + padHeight / 2) 
         {
-        botpad->rect.y -= botpad->speed;
+        botpad->rect.y -= botpad->speed*bothard;
         }
 };
+
+void choosebotmode (){
+    if (IsKeyPressed(KEY_FOUR)) 
+    {
+        bothard = 0.5f;
+        choosedbotmode = true;
+    } else
+    if (IsKeyPressed(KEY_FIVE)) 
+    {
+        bothard = 1.0f;
+        choosedbotmode = true;
+    } else
+    if (IsKeyPressed(KEY_SIX)) 
+    { 
+        bothard = 1.5f;
+        choosedbotmode = true;
+    } else
+    if (IsKeyPressed(KEY_SEVEN)) 
+    {
+        bothard = 2.0f;
+        choosedbotmode = true;
+    } else
+    if (IsKeyPressed(KEY_ZERO)) 
+    {
+        bothard = 10.0f;
+        choosedbotmode = true;
+    }
+    
+}
 
 void choosegamemode (){
     if (IsKeyPressed(KEY_ONE)) 
@@ -104,95 +140,274 @@ void choosegamemode (){
     }
 };
 
-void scoreupdate(){
+void scoreupdateblack(){
     char scoreText[20];
     sprintf(scoreText, "%d - %d", player1score, player2score);
     DrawText(scoreText, screenWidth / 2 - 50, 20, 40, RAYWHITE);
 }
 
+void scoreupdatewhite(){
+    char scoreText[20];
+    sprintf(scoreText, "%d - %d", player1score, player2score);
+    DrawText(scoreText, screenWidth / 2 - 50, 20, 40, BLACK);
+}
+
+    Music soundtrack1;
+    Music soundtrack2;
+    Music soundtrack3;
+void mastervolumn(){
+    if (IsKeyPressed(KEY_M))
+        {
+            if (volume>=0.1f){volume=0;}else {volume+=0.02f;}
+            SetMasterVolume(volume);
+        }
+}
+
+void choosetheme(){
+    if (!choosedtheme)
+    {
+        if (IsKeyPressed(KEY_F))
+        {
+            checkdarkmode=false;
+            choosedtheme=true;
+        } 
+        if (IsKeyPressed(KEY_B)){
+            checkdarkmode=true;
+            choosedtheme=true;
+        }
+    }
+}
+
+void loadsong(){
+    if (IsKeyPressed(KEY_N))
+        {
+            StopMusicStream(soundtrack1);
+            StopMusicStream(soundtrack2);
+            StopMusicStream(soundtrack3);
+            if (songnum==3){songnum=1;}else {songnum++;}
+            switch (songnum){
+                case 1:PlayMusicStream(soundtrack1);break;
+                case 2:PlayMusicStream(soundtrack2);break;
+                case 3:PlayMusicStream(soundtrack3);break;
+            }
+        }
+}
+
 int main(void)
 {
-    
     Paddle paddle1, paddle2;
     Ball ball;
-    Music soundtrack;
-    
-    
     loadgame(&paddle1, &paddle2, &ball);
     InitWindow(screenWidth, screenHeight, "Ping Pong");
     InitAudioDevice();
     
-    soundtrack=LoadMusicStream("ping_pong_sound_track.mp3");
+    soundtrack1=LoadMusicStream("ping_pong_sound_track.mp3");
+    soundtrack2=LoadMusicStream("ballsong.mp3");
+    soundtrack3=LoadMusicStream("goodsong.mp3");
     hitwall=LoadSound("hit wall.wav");
     hitpad=LoadSound("hit pad.wav");
-    PlayMusicStream(soundtrack);
-    SetMusicVolume(soundtrack,0.2f);
     SetTargetFPS(60);
     
     while (!WindowShouldClose())
     {
-        UpdateMusicStream(soundtrack);
-        choosegamemode();
-        
-        if (modechoosed){
-            BeginDrawing();
-                ClearBackground(BLACK);
-                DrawRectangleRec(paddle1.rect, RED);
-                DrawRectangleRec(paddle2.rect, RED);
-                DrawCircleV(ball.pos, ballsize / 2, WHITE);
-            EndDrawing();
-            scoreupdate();
-            if (botmode)
-            {
-                padMove(&paddle1,IsKeyDown(KEY_W), IsKeyDown(KEY_S));
-                runbot (&paddle2,&ball);
+        choosetheme();
+        if (choosedtheme)
+        {
+            mastervolumn();
+            loadsong();
+            UpdateMusicStream(soundtrack1);
+            UpdateMusicStream(soundtrack2);
+            UpdateMusicStream(soundtrack3);
+            if (checkdarkmode)
+            {    
+                choosegamemode();
+                if (modechoosed)
+                {
+                    scoreupdateblack();
+                    if (botmode)
+                    {
+                        choosebotmode();
+                        if (choosedbotmode)
+                        {
+                            BeginDrawing();
+                                ClearBackground(BLACK);
+                                DrawRectangleRec(paddle1.rect, RED);
+                                DrawRectangleRec(paddle2.rect, RED);
+                                DrawCircleV(ball.pos, ballsize / 2, WHITE);
+                            EndDrawing();
+                            padMove(&paddle1,IsKeyDown(KEY_W), IsKeyDown(KEY_S));
+                            runbot (&paddle2,&ball);
+                        } 
+                        else 
+                        {
+                            BeginDrawing();
+                                ClearBackground(BLACK);
+                                DrawText("Ping Pong", screenWidth / 2 - 120, screenHeight / 4 - 60, 50, RAYWHITE);
+                                DrawText("Press '4' easy Mode", screenWidth / 2 - 140, screenHeight / 2 + 60, 20, RAYWHITE);
+                                DrawText("Press '5' medium Mode", screenWidth / 2 - 140, screenHeight / 2 + 90, 20, RAYWHITE);
+                                DrawText("Press '6' hard Mode", screenWidth / 2 - 140, screenHeight / 2 + 120, 20, RAYWHITE);
+                                DrawText("Press '7' nightmate Mode", screenWidth / 2 - 140, screenHeight / 2 + 150, 20, RAYWHITE);
+                                DrawText("Press '0' why even bother", screenWidth / 2 - 140, screenHeight / 2 + 180, 20, RAYWHITE);
+                            EndDrawing();
+                        };
+                    } 
+                    else 
+                    {
+                        BeginDrawing();
+                            ClearBackground(BLACK);
+                            DrawRectangleRec(paddle1.rect, RED);
+                            DrawRectangleRec(paddle2.rect, RED);
+                            DrawCircleV(ball.pos, ballsize / 2, WHITE);
+                        EndDrawing();
+                        padMove(&paddle1,IsKeyDown(KEY_W), IsKeyDown(KEY_S));
+                        padMove(&paddle2,IsKeyDown(KEY_UP), IsKeyDown(KEY_DOWN));
+                    };
+                    
+                    if ((botmode&&choosedbotmode)||!botmode){
+                        if (player1score >= 10 || player2score >= 10)
+                        {
+                            if (player1score >= 10) 
+                            {
+                                DrawText("Player 1 Win", screenWidth / 2 - 100, screenHeight / 2 - 30, 50, RAYWHITE);
+                            } else 
+                            {
+                                DrawText("Player 2 Win", screenWidth / 2 - 100, screenHeight / 2 - 30, 50, RAYWHITE);
+                            }
+                            DrawText("Press R to Restart", screenWidth / 2 - 120, screenHeight / 2 + 30, 20, RAYWHITE);
+                            if (IsKeyPressed(KEY_R)) {
+                                player1score = 0;
+                                player2score = 0;
+                                speedup=1;
+                            }
+                        } else 
+                        {
+                            ballMove(&ball, &paddle1, &paddle2);
+                        }
+                    }
+                    //choose mode again
+                    if (IsKeyDown(KEY_L))
+                    {
+                           modechoosed = false;
+                           player1score = 0;
+                           player2score = 0;
+                           speedup=1;
+                           choosedbotmode=false;
+                    }
+                } 
+                else 
+                {
+                BeginDrawing();
+                    ClearBackground(BLACK);
+                    DrawText("Ping Pong", screenWidth / 2 - 120, screenHeight / 4 - 60, 50, RAYWHITE);
+                    DrawText("Press '1' for Friend Mode", screenWidth / 2 - 140, screenHeight / 2 + 90, 20, RAYWHITE);
+                    DrawText("Press '2' for Bot Mode", screenWidth / 2 - 140, screenHeight / 2 + 110, 20, RAYWHITE);
+                    DrawText("Press 'L' to choose mode again", 20, 430, 10, RAYWHITE);
+                    DrawText("Press 'M' to mute sound", 670, 430, 10, RAYWHITE);
+                    DrawText("Press 'N' to choose song", 350, 430, 10, RAYWHITE);
+                EndDrawing();
+                }
             } 
-            else 
+            else
             {
-                padMove(&paddle1,IsKeyDown(KEY_W), IsKeyDown(KEY_S));
-                padMove(&paddle2,IsKeyDown(KEY_UP), IsKeyDown(KEY_DOWN));
-            };
-            if (player1score >= 10 || player2score >= 10)
-            {
-                if (player1score >= 10) 
+                choosegamemode();
+                if (modechoosed)
                 {
-                    DrawText("Player 1 Win", screenWidth / 2 - 100, screenHeight / 2 - 30, 50, RAYWHITE);
-                } else 
+                   
+                    scoreupdatewhite();
+                    if (botmode)
+                    {
+                        choosebotmode();
+                        if (choosedbotmode)
+                        {
+                        BeginDrawing();
+                            ClearBackground(WHITE);
+                            DrawRectangleRec(paddle1.rect, RED);
+                            DrawRectangleRec(paddle2.rect, RED);
+                            DrawCircleV(ball.pos, ballsize / 2, BLACK);
+                        EndDrawing();
+                        padMove(&paddle1,IsKeyDown(KEY_W), IsKeyDown(KEY_S));
+                        runbot (&paddle2,&ball);
+                        }
+                        else
+                        {
+                            BeginDrawing();
+                                ClearBackground(WHITE);
+                                DrawText("Ping Pong", screenWidth / 2 - 120, screenHeight / 4 - 60, 50, BLACK);
+                                DrawText("Press '4' easy Mode", screenWidth / 2 - 140, screenHeight / 2 + 60, 20, BLACK);
+                                DrawText("Press '5' medium Mode", screenWidth / 2 - 140, screenHeight / 2 + 90, 20, BLACK);
+                                DrawText("Press '6' hard Mode", screenWidth / 2 - 140, screenHeight / 2 + 120, 20, BLACK);
+                                DrawText("Press '7' nightmate Mode", screenWidth / 2 - 140, screenHeight / 2 + 150, 20, BLACK);
+                                DrawText("Press '0' why even bother", screenWidth / 2 - 140, screenHeight / 2 + 180, 20, BLACK);
+                            EndDrawing();                         
+                        }
+                    } 
+                    else 
+                    {
+                        BeginDrawing();
+                            ClearBackground(WHITE);
+                            DrawRectangleRec(paddle1.rect, RED);
+                            DrawRectangleRec(paddle2.rect, RED);
+                            DrawCircleV(ball.pos, ballsize / 2, BLACK);
+                        EndDrawing();
+                        padMove(&paddle1,IsKeyDown(KEY_W), IsKeyDown(KEY_S));
+                        padMove(&paddle2,IsKeyDown(KEY_UP), IsKeyDown(KEY_DOWN));
+                    };
+                    
+                    if ((botmode&&choosedbotmode)||!botmode){
+                        if (player1score >= 10 || player2score >= 10)
+                        {
+                            if (player1score >= 10) 
+                            {
+                                DrawText("Player 1 Win", screenWidth / 2 - 100, screenHeight / 2 - 30, 50, BLACK);
+                            } else 
+                            {
+                                DrawText("Player 2 Win", screenWidth / 2 - 100, screenHeight / 2 - 30, 50, BLACK);
+                            }
+                            DrawText("Press R to Restart", screenWidth / 2 - 120, screenHeight / 2 + 30, 20, BLACK);
+                            if (IsKeyPressed(KEY_R)) {
+                                player1score = 0;
+                                player2score = 0;
+                                speedup=1;
+                            }
+                        } else 
+                        {
+                            ballMove(&ball, &paddle1, &paddle2);
+                        }
+                    }
+                    //choose mode again
+                    if (IsKeyDown(KEY_L))
+                    {
+                           modechoosed = false;
+                           player1score = 0;
+                           player2score = 0;
+                           speedup=1;
+                    }
+                } 
+                else 
                 {
-                    DrawText("Player 2 Win", screenWidth / 2 - 100, screenHeight / 2 - 30, 50, RAYWHITE);
+                BeginDrawing();
+                    ClearBackground(WHITE);
+                    DrawText("Ping Pong", screenWidth / 2 - 120, screenHeight / 4 - 60, 50, BLACK);
+                    DrawText("Press '1' for Friend Mode", screenWidth / 2 - 140, screenHeight / 2 + 90, 20, BLACK);
+                    DrawText("Press '2' for Bot Mode", screenWidth / 2 - 140, screenHeight / 2 + 110, 20, BLACK);
+                    DrawText("Press 'L' to choose mode again", 20, 430, 10, BLACK);
+                    DrawText("Press 'M' to mute sound", 670, 430, 10, BLACK);
+                    DrawText("Press 'N' to choose song", 350, 430, 10, BLACK);
+                EndDrawing();
                 }
-                DrawText("Press R to Restart", screenWidth / 2 - 120, screenHeight / 2 + 30, 20, RAYWHITE);
-                if (IsKeyPressed(KEY_R)) {
-                    player1score = 0;
-                    player2score = 0;
-                    speedup=1;
-                }
-            } else 
-            {
-                ballMove(&ball, &paddle1, &paddle2);
-            }
-            //choose mode again
-            if (IsKeyDown(KEY_L))
-            {
-                   modechoosed = false;
-                   player1score = 0;
-                   player2score = 0;
-                   speedup=1;
             }
         } 
-        else 
+        else
         {
-        BeginDrawing();
-            ClearBackground(BLACK);
-            DrawText("Ping Pong", screenWidth / 2 - 120, screenHeight / 4 - 60, 50, RAYWHITE);
-            DrawText("Press '1' for Friend Mode", screenWidth / 2 - 140, screenHeight / 2 + 100, 20, RAYWHITE);
-            DrawText("Press '2' for Bot Mode", screenWidth / 2 - 140, screenHeight / 2 + 130, 20, RAYWHITE);
-            DrawText("Press 'L' to choose mode again", 20, 430, 10, RAYWHITE);
-        EndDrawing();
+            BeginDrawing();
+                    ClearBackground(BLACK);
+                    DrawText("Ping Pong", screenWidth / 2 - 120, screenHeight / 4 - 60, 50, WHITE);
+                    DrawText("Press 'F' for light mode", screenWidth / 2 - 140, screenHeight / 2 + 90, 20, WHITE);
+                    DrawText("Press 'B' for dark mode", screenWidth / 2 - 140, screenHeight / 2 + 110, 20, WHITE);
+                EndDrawing();
         }
-        
     }
-    UnloadMusicStream(soundtrack);
+    UnloadMusicStream(soundtrack1);
     CloseAudioDevice();
     CloseWindow();
     return 0;
